a657ac45737caa410d89875bc2507c1dd94e496a
LeislaCV <you@example.com>
2026-03-30 16:52:52 -0600

    yaaaaaaaaaaaaaaaaa

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mssql+pymssql://leisla_SQLLogin_1:wg6t668kt2@gestion-pacientes.mssql.somee.com/gestion-pacientes"
+    SQLALCHEMY_DATABASE_URI = "mssql+pymssql://leisla_SQLLogin_1:wg6t668kt2@gestion-pacientes.mssql.somee.com/gestion-pacientes" 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = "mssql+pymssql://leisla_SQLLogin_1:wg6t668kt2@gestion-pacientes.mssql.somee.com/gestion-pacientes?driver=ODBC+Driver+17+for+SQL+Server" 
+   # SQLALCHEMY_DATABASE_URI = "mssql+pymssql://leisla_SQLLogin_1:wg6t668kt2@gestion-pacientes.mssql.somee.com/gestion-pacientes?driver=ODBC+Driver+17+for+SQL+Server" 
