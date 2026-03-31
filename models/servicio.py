85cfff78708714ea7eb1a4ca155b888af76804f9
LeislaCV <you@example.com>
2026-03-31 11:10:10 -0600

    Cambio models servicios, (relación)

--- models/servicio.py
+++ models/servicio.py
@@ -4,9 +4,8 @@ class Servicio(db.Model):
     __tablename__ = 'Servicio'
 
     id = db.Column(db.Integer, primary_key=True)
-    paciente_id = db.Column(db.Integer, nullable=False)  # ⚠️ quitamos FK inválida
     nombre_servicio = db.Column(db.String(150), nullable=False)
     precio = db.Column(db.Float, nullable=False)
-    fecha = db.Column(db.Date, nullable=False)
 
-    citas = db.relationship('Cita', backref='servicio', lazy=True)
+
+
